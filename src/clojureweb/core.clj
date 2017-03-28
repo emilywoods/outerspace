a424ec032f93c58543a1f53021ae860037149af4
Emily Woods <emilymonica.woods@gmail.com>
2017-03-28 22:32:23 +0100

    Make request to SHARE api

--- src/clojureweb/core.clj
+++ src/clojureweb/core.clj
@@ -1,8 +1,20 @@
 (ns clojureweb.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as client]
+            [cheshire.core :as json]))
+
+(defn get-api
+  [path params]
+  (let [url (str "https://share.osf.io/api/v2/search/" path)
+        query-params (merge params {:format "json" :per_page 10})
+        response (json/parse-string (:body (client/get url {:query-params query-params})) true)
+        metadata (first response)
+        results (second response) ]
+    {:metadata metadata
+     :results results}))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
-
+  (get-api "creativeworks/_search" {:page 1})
+  )
